f37d84a1de5572f3e3d3412c97c276f49388aee7
Hamzah Noer Arifin <you@example.com>
2025-12-24 08:46:25 +0700

    24-12-2025

--- onedatahr/app/Helpers/MenuHelper.php
+++ onedatahr/app/Helpers/MenuHelper.php
@@ -49,8 +49,25 @@ class MenuHelper
                 'name' => 'Laporan',
                 'path' => '/laporan',
             ],
+            [
+                'icon' => 'charts',
+                'name' => 'KPI Karyawan', // Nama menu diperjelas
+                'path' => '/kpi/dashboard', // <-- Link baru ke halaman index
+            ],
         ];
     }
+      // --- [BARU] Menambahkan Menu Khusus Performance (KPI) ---
+    public static function getPerformanceItems()
+    {
+        return [
+            [
+                'icon' => 'charts',
+                'name' => 'KPI Karyawan', // Nama menu diperjelas
+                'path' => '/kpi/dashboard', // <-- Link baru ke halaman index
+            ],
+        ];
+    }
+
 
     public static function getOthersItems()
     {
@@ -122,5 +139,5 @@ class MenuHelper
 
         return $icons[$iconName] ?? '<svg width="1em" height="1em" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z" fill="currentColor"/></svg>';
     }
-    
+
 }
